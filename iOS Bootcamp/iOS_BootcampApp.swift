8f97d4f91f37ba8c83a36ba0ce1e3e985ec491b8
rihansr <rx23026@gmail.com>
2023-10-09 11:43:05 +0600

    Initial Commit

---
+++ iOS Bootcamp/iOS_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_BootcampApp.swift
+//  iOS Bootcamp
+//
+//  Created by Macuser on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
