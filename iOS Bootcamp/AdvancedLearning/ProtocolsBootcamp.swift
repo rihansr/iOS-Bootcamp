350cc186603efdb0c2b89b1eb0cdec23d920509a
rihansr <rx23026@gmail.com>
2023-10-16 15:55:47 +0600

    Advanced Learning: Protocols

---
+++ iOS Bootcamp/AdvancedLearning/ProtocolsBootcamp.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+protocol ColorTheme: PrimaryColorTheme, SecondaryColorTheme {}
+
+protocol PrimaryColorTheme {
+    var primary: Color { get }
+    var secondary: Color { get }
+    var tertiary: Color { get }
+}
+
+protocol SecondaryColorTheme {
+    var surface: Color { get }
+    var action: Color { get }
+}
+
+struct LightColorTheme: ColorTheme {
+    var primary: Color = .yellow
+    var secondary: Color = .black
+    var tertiary: Color = .white
+    var surface: Color = .purple
+    var action: Color = .green
+}
+
+struct DarkColorTheme: ColorTheme {
+    var primary: Color = .blue
+    var secondary: Color = .white
+    var tertiary: Color = .black
+    var surface: Color = .red
+    var action: Color = .gray
+}
+
+struct ProtocolsBootcamp: View {
+    
+    let theme: ColorTheme
+    
+    var body: some View {
+        ZStack{
+            theme.tertiary.ignoresSafeArea()
+            Text("Press Me!!")
+                .font(.headline)
+                .foregroundColor(theme.secondary)
+                .padding(.all)
+                .background(theme.primary)
+                .cornerRadius(6)
+        }
+    }
+}
+
+struct ProtocolsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ProtocolsBootcamp(theme: LightColorTheme())
+    }
+}
