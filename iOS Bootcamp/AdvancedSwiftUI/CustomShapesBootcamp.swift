565979ffd72e1d55467c28af3cad0fa0a298d83f
rihansr <rx23026@gmail.com>
2023-11-10 16:05:43 +0600

    Advanced Swift UI: Custom shapes with Arcs and Quad Curves

--- iOS Bootcamp/AdvancedSwiftUI/CustomShapesBootcamp.swift
+++ iOS Bootcamp/AdvancedSwiftUI/CustomShapesBootcamp.swift
@@ -5,6 +5,17 @@
 //  Created by Macuser on 10/11/2023.
 //
 
+/*
+ (minX,minY) * (midX,minY) * (maxX,minY)
+  * * * * * * * * * * * * * * * * * * *
+  * * * * * * * * * * * * * * * * * * *
+ (minx,midy) * (midX,midY) * (maxX,midY)
+  * * * * * * * * * * * * * * * * * * *
+  * * * * * * * * * * * * * * * * * * *
+ (minX,maxY) * (midX,maxY) * (maxX,maxY)
+ */
+
+
 import SwiftUI
 
 struct Triangle: Shape {
@@ -44,28 +55,103 @@ struct Trapezoid: Shape {
     }
 }
 
+struct PacManArc: Shape{
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.width/2,
+                startAngle: Angle(degrees: -20),
+                endAngle: Angle(degrees: 20),
+                clockwise: true
+            )
+        }
+    }
+}
+
+struct Arc: Shape{
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addArc(
+                center: CGPoint(x: rect.midX, y: rect.midY),
+                radius: rect.width/2,
+                startAngle: Angle(degrees: 0),
+                endAngle: Angle(degrees: 180),
+                clockwise: false
+            )
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        }
+    }
+}
+
+struct Quad: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.maxX, y:  rect.minY)
+            )
+        }
+    }
+}
+
+struct Water: Shape{
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addQuadCurve(
+                to: CGPoint(x: rect.midX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.25, y: rect.height * 0.40)
+            )
+            path.addQuadCurve(
+                to: CGPoint(x: rect.maxX, y: rect.midY),
+                control: CGPoint(x: rect.width * 0.75, y: rect.height * 0.60)
+            )
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        }
+    }
+}
+
 struct CustomShapesBootcamp: View {
     var body: some View {
-        ZStack{
-            //Triangle()
-            //Diamond()
-            Trapezoid()
-                /*
-                 .stroke(style: StrokeStyle(
-                     lineWidth: 3, lineCap: .round, dash: [10]
-                 ))
-                 */
-                .fill(.red)
-                //.foregroundColor(.blue)
-                .frame(width: 200, height: 100)
-            
+        //Triangle()
+        //Diamond()
+        //Trapezoid()
+        //PacManArc()
+        //Arc()
+        //Quad()
+        Water()
             /*
-             Image("png_image")
-                 .resizable()
-                 .scaledToFit()
-                 .clipShape(Triangle())
+             .stroke(style: StrokeStyle(
+                 lineWidth: 3, lineCap: .round, dash: [10]
+             ))
              */
-        }
+            .fill(
+                LinearGradient(
+                    colors: [.teal, .cyan, .blue],
+                    startPoint: .topLeading, endPoint: .bottomTrailing
+                )
+            )
+            //.foregroundColor(.blue)
+            //.frame(width: 200, height: 100)
+            .frame(maxWidth: .infinity)
+            .ignoresSafeArea()
+        
+        /*
+         Image("png_image")
+             .resizable()
+             .scaledToFit()
+             .clipShape(Triangle())
+         */
     }
 }
 
