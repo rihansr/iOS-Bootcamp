01eba824a9ca26087afd4481f738437fd07add2d
rihansr <rx23026@gmail.com>
2023-11-10 14:06:49 +0600

    Advanced Swift UI: Custom Shapes

---
+++ iOS Bootcamp/AdvancedSwiftUI/CustomShapesBootcamp.swift
@@ -0,0 +1,76 @@
+//
+//  CustomShapesBootcamp.swift
+//  iOS Bootcamp
+//
+//  Created by Macuser on 10/11/2023.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxX))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
+
+struct CustomShapesBootcamp: View {
+    var body: some View {
+        ZStack{
+            //Triangle()
+            //Diamond()
+            Trapezoid()
+                /*
+                 .stroke(style: StrokeStyle(
+                     lineWidth: 3, lineCap: .round, dash: [10]
+                 ))
+                 */
+                .fill(.red)
+                //.foregroundColor(.blue)
+                .frame(width: 200, height: 100)
+            
+            /*
+             Image("png_image")
+                 .resizable()
+                 .scaledToFit()
+                 .clipShape(Triangle())
+             */
+        }
+    }
+}
+
+struct CustomShapesBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomShapesBootcamp()
+    }
+}
